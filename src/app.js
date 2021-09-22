6288f0a6608ef79ee3d666853c58433843591bf8
marcof90 <ingmarcosanabria@gmail.com>
2021-09-21 19:35:18 -0500

    Procfile

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 const indexRoutes = require('./routes/index');
-const port = 3000;
 
 //configuraciones
+app.set('port', process.env.PORT || 3000);
 mongoose.connect('mongodb+srv://root:toor@cluster58.xuwzi.mongodb.net/Cluster58?retryWrites=true&w=majority')
 .then(db => console.log('Connected'))
 .catch(err => console.log(err));
@@ -17,6 +17,6 @@ app.use(express.urlencoded({extended: false}));
 app.use('/items/', indexRoutes);
 
 //inicializacion del server
-app.listen(port, ()=>{
+app.listen(app.get('port'), ()=>{
     console.log('Server started!');
 });
